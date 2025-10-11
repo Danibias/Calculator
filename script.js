e2d0249b7d18a4ba2a52d7f5cbd2d67c6b8e2d5c
Danibias <danielebiasetti1@gmail.com>
2025-10-11 10:32:52 +0100

    Operate fuction created

--- script.js
+++ script.js
@@ -2,22 +2,34 @@
  * BASIC FUNCTIONS
  */
 
-// Add
-const add = (...numbers) => {
-    return numbers.reduce((total, num) => total + num, 0);
-}
+// // Add
+// const add = (...numbers) => {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
  
-// Subtract
-const subtract = (...numbers) => {
-    return numbers.reduce((total, num) => total - num, 0);
-}
+// // Subtract
+// const subtract = (...numbers) => {
+//     return numbers.reduce((total, num) => total - num, 0);
+// }
 
-// Multiply
-const multiply = (...numbers) => {
-    return numbers.reduce((total, num) => total * num, 1);
-}
+// // Multiply
+// const multiply = (...numbers) => {
+//     return numbers.reduce((total, num) => total * num, 1);
+// }
 
-// Divide
-const divide = (...numbers) => {
-    return numbers.reduce((total, num) => total / num, 1);
+// // Divide
+// const divide = (...numbers) => {
+//     return numbers.reduce((total, num) => total / num, 1);
+// }
+
+const operations = {
+    '+': (a,b) => a + b,
+    '-': (a,b) => a - b,
+    '*': (a,b) => a * b,
+    '/': (a,b) => (b === 0 ? "Can't divide by 0": a / b)
+};
+
+const operate = (operator, num1, num2) => {
+    const operation = operations[operator];
+    return operation ? operation(num1, num2) : "Invalid operator";
 }
