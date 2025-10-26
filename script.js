5291303c1ce06a0b667711523dd8dd509398215a
Danibias <danielebiasetti1@gmail.com>
2025-10-26 10:25:37 +0000

    css changed

--- script.js
+++ script.js
@@ -2,26 +2,6 @@
  * BASIC FUNCTIONS
  */
 
-// // Add
-// const add = (...numbers) => {
-//     return numbers.reduce((total, num) => total + num, 0);
-// }
- 
-// // Subtract
-// const subtract = (...numbers) => {
-//     return numbers.reduce((total, num) => total - num, 0);
-// }
-
-// // Multiply
-// const multiply = (...numbers) => {
-//     return numbers.reduce((total, num) => total * num, 1);
-// }
-
-// // Divide
-// const divide = (...numbers) => {
-//     return numbers.reduce((total, num) => total / num, 1);
-// }
-
 const operations = {
     '+': (a,b) => a + b,
     '-': (a,b) => a - b,
@@ -33,3 +13,36 @@ const operate = (operator, num1, num2) => {
     const operation = operations[operator];
     return operation ? operation(num1, num2) : "Invalid operator";
 }
+
+// Stores what's shown on the calculator screen
+let displayValue = "";
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
+const display = document.querySelector(".display");
+
+const updateDisplay = () => {
+    display.textContent = displayValue || "0"; // show 0 if empty
+}
+
+const digits = document.querySelectorAll(".digit");
+
+digits.forEach(button => {
+    button.addEventListener("click", () => {
+        const digit = button.textContent;
+        // show numeric characters only
+        if (!isNaN(digit) || digit === ".") {
+            displayValue += digit;
+            updateDisplay();
+        }
+    });
+});
+
+// clear buttons
+document.getElementById("clearAll").addEventListener("click", () => {
+    displayValue = "";
+    display.textContent = 0;
+});
+
+
